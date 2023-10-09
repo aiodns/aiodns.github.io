d73c584aa67a955557ddc3a6a3971a9e36d95cca
stand <135704868+aiodns@users.noreply.github.com>
2023-10-09 20:26:58 +0300

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -1,1 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdown = document.querySelector(".dropdown");
+    const dropdownContent = document.querySelector(".dropdown-content");
+
+    dropdown.addEventListener("click", function() {
+        dropdownContent.classList.toggle("show");
+    });
+
+    // Close dropdown when clicking outside
+    window.addEventListener("click", function(event) {
+        if (!event.target.matches(".dropdown-btn")) {
+            if (dropdownContent.classList.contains("show")) {
+                dropdownContent.classList.remove("show");
+            }
+        }
+    });
+
+    // Handle checkbox selections
+    const checkboxes = dropdownContent.querySelectorAll("input[type='checkbox']");
+    checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener("change", function() {
+            const selectedOptions = Array.from(checkboxes)
+                .filter(function(checkbox) {
+                    return checkbox.checked;
+                })
+                .map(function(checkbox) {
+                    return checkbox.value;
+                });
+
+            dropdown.querySelector(".dropdown-btn").textContent = selectedOptions.length > 0
+                ? selectedOptions.join(", ")
+                : "Select an option";
+        });
+    });
+});
 
