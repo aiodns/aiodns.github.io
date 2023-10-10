a5b6a3409d705ac21d9143a08c5721761c3076f2
stand <135704868+aiodns@users.noreply.github.com>
2023-10-10 08:58:53 +0300

    Update searchbar.js

--- searchbar.js
+++ searchbar.js
@@ -1,1 +1,17 @@
+function toggleSearchBox() {
+            var searchBox = document.getElementById("searchBox");
+            if (searchBox.style.display === "block") {
+                searchBox.style.display = "none";
+            } else {
+                searchBox.style.display = "block";
+            }
+        }
 
+        // Close the search box when clicking outside of it
+        window.addEventListener("click", function(event) {
+            var searchContainer = document.querySelector(".search-container");
+            var searchBox = document.getElementById("searchBox");
+            if (!searchContainer.contains(event.target)) {
+                searchBox.style.display = "none";
+            }
+        });
